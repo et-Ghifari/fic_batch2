a2005bb1e328f1b8f1066061deaf9d10e04fb8d2
et-Ghifari <et.ghifari@gmail.com>
2023-02-15 09:54:42 +0700

    tugas 1

--- lib/fit_meet1/tugas1.dart
+++ lib/fit_meet1/tugas1.dart
@@ -9,13 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Tugas 1'),
+        appBar: AppBar(
+          title: const Text('Tugas 1'),
+        ),
+        body: const Center(
+          child: ChangeText(),
         ),
       ),
+    );
+  }
+}
+
+class ChangeText extends StatefulWidget {
+  const ChangeText({super.key});
+
+  @override
+  State<ChangeText> createState() => _ChangeTextState();
+}
+
+class _ChangeTextState extends State<ChangeText> {
+  DateTime time = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Text : $time'),
+        ElevatedButton(
+          onPressed: () {
+            time = DateTime.now();
+            setState(() {});
+          },
+          child: const Text('Update'),
+        ),
+      ],
     );
   }
 }
